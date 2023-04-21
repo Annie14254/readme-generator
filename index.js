2e9b6f68b02ce1242d37f81e595708c78d4860a1
unknown <annieyashley@gmail.com>
2023-04-21 14:49:32 -0500

    setting up the markdown structure

--- index.js
+++ index.js
@@ -1,13 +1,74 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: 'What is your GitHub username?',
+        name: 'username' 
+    },
+    {
+        type: "input",
+        message: 'What is your email?',
+        name: 'email' 
+    },
+    {
+        type: "input",
+        message: 'What is the name of the repository?',
+        name: 'title' 
+    },
+    {
+        type: "input",
+        message: 'Provide a short description of the repository.',
+        name: 'description' 
+    },
+    {
+        type: "list",
+        message: 'What is the license of the repository?',
+        choices: ["MIT", "GPLv3", "Apache_2.0", "CC0_1.0"], 
+        name: "license",
+    },
+    {
+        type: "input",
+        message: 'What command should the user use to install dependencies?',
+        name: 'dependencies' 
+    },
+    {
+        type: "input",
+        message: 'What command should the user use to run tests?',
+        name: 'test' 
+    },
+    {
+        type: "input",
+        message: 'What should the user know about using the repository?',
+        name: 'usage' 
+    },
+    {
+        type: "input",
+        message: 'What should the user know about contributing to the repository?',
+        name: 'contribution' 
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    const fileName = "./README.md"
+    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
+        err ? console.error(err) : console.log('README generated!'))
+
+}
+// fuck my life
 
 // TODO: Create a function to initialize app
-function init() {}
+function init(){
+    inquirer.prompt(questions)
+    .then((data) => {
+        writetoFile(generateMarkdown(data));
+    })
+}
 
 // Function call to initialize app
-init();
+//init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  var licenseBadge
+
+  if(license){
+    licenseBadge = `[![License Badge](https://img.shields.io/badge/License-${license}-green.svg)`
+  }
+
+  return licenseBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +21,34 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+    
+    ${renderLicenseSection()}
+
+    ## Description
+    ${data.description}
+
+    ## Installation
+    To install the dependencies required for the application, run the following line in the terminal.
+
+    \`\`\`
+    ${data.dependencies}
+    \`\`\`
+
+    ## Usage
+    ${data.usage}
+
+    ## Testing
+    To test the application, run the following line in the terminal.
+
+    \`\`\`
+    ${data.test}
+    \`\`\`
+    
+    ## Contribution
+    ${data.contribution}
+
+    ## Contact
+    If you have questions, contact the author of the repository, ${data.username}, at ${data.email}.
 
 `;
 }
